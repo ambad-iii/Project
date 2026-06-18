c6a5d3231575ea3cc253d1a0d3d6b84144096da6
Afrah1712 <afrahshaji2005@gmail.com>
2026-06-18 13:11:41 +0530

    Update and rename app.py to admin/app.py

--- admin/app.py
+++ admin/app.py
@@ -1,14 +1,6 @@
 from flask import Flask, render_template,request,redirect
 app = Flask(__name__)
-@app.route('/', methods=['GET','POST'])
-def login():
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == "admin" and password == "admin":
-            return redirect('/dashboard')
-    return render_template('login.html')
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
